42e0cc1b071ff9dd9c767a4d8cf20ad25a6ea20c
RAbdulsalam <rasakdare6@gmail.com>
2024-04-18 07:06:05 -0500

    mod6-project

--- get_places1.ps1
+++
@@ -1,5 +0,0 @@
-$databasePath = "TSPD/j3uv3vkf.default/places.sqlite"
-$tableName = "moz_places"
-$outputPath = "out1.csv"
-
-& C:\sqlite\sqlite3 -header -csv $databasePath "SELECT * FROM $tableName" > $outputPath
